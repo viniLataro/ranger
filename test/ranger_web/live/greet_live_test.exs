0d02c483d426b78e242eef3a7a3aac1d50324ff8
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-07-21 15:17:34 -0300

    testing rendering

--- test/ranger_web/live/greet_live_test.exs
+++ test/ranger_web/live/greet_live_test.exs
@@ -1,5 +1,35 @@
 defmodule RangerWeb.GreetLiveTest do
+  @moduledoc false
+
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "rendering disconnected state", %{conn: conn} do
+    disconnected = conn |> get(~p"/greet")
+
+    assert html_response(disconnected, 200) =~ "Welcome to stateless HTTP"
+  end
+
+  test "upgrades the connection", %{conn: conn} do
+    disconnected = conn |> get(~p"/greet")
+
+    {:ok, _view, html} = live(disconnected)
+
+    assert html =~ "Welcome to Testing LiveView"
+  end
+
+  test "rendering connected state", %{conn: conn} do
+    {:ok, _view, html} = live(conn, ~p"/greet")
+
+    assert html =~ "Welcome to Testing LiveView"
+  end
+
+  test "rendering with the view", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/greet")
+
+    assert view.module == RangerWeb.GreetLive
+    assert is_pid(view.pid)
+    assert render(view) =~ "Welcome to Testing LiveView"
+  end
 end
