1dfd20380ff8f1d47521be460ecd2051fbffe78a
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-07-21 15:51:01 -0300

    scoping assertions with has_element?/3

--- test/ranger_web/live/avatar_live_test.exs
+++ test/ranger_web/live/avatar_live_test.exs
@@ -1,5 +1,35 @@
 defmodule RangerWeb.AvatarLiveTest do
+  @moduledoc false
+
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  alias Ranger.Gravatar
+
+  test "renders avatar for given email", %{conn: conn} do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, _view, html} = live(conn, ~p"/avatar/#{email}")
+
+    assert html =~ avatar_url
+  end
+
+  test "renders avatar image from given email", %{conn: conn} do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, view, html} = live(conn, ~p"/avatar/#{email}")
+
+    assert has_element?(view, ~s(img[src*="#{avatar_url}"]))
+  end
+
+  test "renders avatar image from given email better way", %{conn: conn} do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, view, html} = live(conn, ~p"/avatar/#{email}")
+
+    avatar = element(view, ~s(img[src*="#{avatar_url}"]))
+
+    assert has_element?(avatar)
+  end
 end
