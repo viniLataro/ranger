6d2ff6329589ed0347976957f7a8c62ad97980de
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-07-24 16:25:54 -0300

    testing live components

--- test/ranger_web/live/settings_live/editable_input_component_test.exs
+++ test/ranger_web/live/settings_live/editable_input_component_test.exs
@@ -4,4 +4,12 @@ defmodule RangerWeb.SettingsLive.EditableInputComponentTest do
   import Phoenix.LiveViewTest
 
   alias RangerWeb.SettingsLive.EditableInputComponent
+
+  test "renders field text" do
+    user = %{name: "Frodo"}
+
+    html = render_component(EditableInputComponent, id: "name", field: :name, user: user)
+
+    assert html =~ "Frodo"
+  end
 end
--- test/ranger_web/live/settings_live_test.exs
+++ test/ranger_web/live/settings_live_test.exs
@@ -1,5 +1,56 @@
 defmodule RangerWeb.SettingsLiveTest do
+  @moduledoc false
+
   use RangerWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
+
+  alias Ranger.Repo
+  alias Ranger.User
+
+  test "renders user's information", %{conn: conn} do
+    user = create_user()
+
+    {:ok, _view, html} = live(conn, ~p"/users/#{user}/settings")
+
+    assert html =~ user.name
+    assert html =~ user.email
+  end
+
+  test "users can edit their name", %{conn: conn} do
+    user = create_user(%{name: "Frodo"})
+    {:ok, view, _html} = live(conn, ~p"/users/#{user}/settings")
+
+    view
+    |> element("#name")
+    |> render_click()
+
+    view
+    |> form("#name-form", %{name: "Bilbo"})
+    |> render_submit()
+
+    assert has_element?(view, "#name", "Bilbo")
+  end
+
+  test "users can edit their email", %{conn: conn} do
+    user = create_user%{email: "frodo@example.com"}
+    {:ok, view, _html} = live(conn, ~p"/users/#{user}/settings")
+
+    view
+    |> element("#email")
+    |> render_click()
+
+    view
+    |> form("#email-form", %{email: "frodo@fellowship.com"})
+    |> render_submit()
+
+    assert has_element?(view, "#email", "frodo@fellowship.com")
+  end
+
+  defp create_user(attrs \\ %{}) do
+    %{name: "somename", email: "somemail@example.com"}
+    |> Map.merge(attrs)
+    |> User.changeset()
+    |> Repo.insert!()
+  end
 end
