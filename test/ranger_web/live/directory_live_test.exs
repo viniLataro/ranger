a9e7855f081c62b75a350ab8d2fa81a3cad7604d
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-07-24 22:27:43 -0300

    testing live patch

--- test/ranger_web/live/directory_live_test.exs
+++ test/ranger_web/live/directory_live_test.exs
@@ -4,4 +4,32 @@ defmodule RangerWeb.DirectoryLiveTest do
   import Phoenix.LiveViewTest
 
   alias Ranger.Directory
+
+  test "user is directed to member information", %{conn: conn} do
+    member = Directory.get_by(name: "Aragorn")
+    {:ok, view, _html} = live(conn, ~p"/directory")
+
+    view
+    |> element("[data-role=member]", "Aragorn")
+    |> render_click()
+
+    assert_patch(view, ~p"/directory/#{member.id}")
+  end
+
+    test "user can see member information upon selection", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/directory")
+
+    view
+    |> element("[data-role=member]", "Aragorn")
+    |> render_click()
+
+    assert has_element?(view, "#active-member", "Aragorn")
+  end
+
+  test "user can visit specific member", %{conn: conn} do
+    member = Directory.get_by(name: "Aragorn")
+    {:ok, view, _html} = live(conn, ~p"/directory/#{member.id}")
+
+    assert has_element?(view, "#active-member", "Aragorn")
+  end
 end
