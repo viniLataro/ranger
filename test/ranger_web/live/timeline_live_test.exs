dcd6c228af7f7fa1080f02d47f648f4500ff2538
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-07-24 23:01:27 -0300

    testing JS hooks

--- test/ranger_web/live/timeline_live_test.exs
+++ test/ranger_web/live/timeline_live_test.exs
@@ -1,7 +1,21 @@
 defmodule RangerWeb.TimelineLiveTest do
+  @moduledoc false
+
   use RangerWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
 
   alias Ranger.Timeline
+
+  test "user can see older posts with infinite scroll", %{conn: conn} do
+    [post] = Timeline.posts(offset: 10, limit: 1)
+    {:ok, view, _html} = live(conn, ~p"/timeline")
+
+    view
+    |> element("#load-more", "Loading ...")
+    |> render_hook("load-more")
+
+    assert has_element?(view, "[data-role=author]", post.author)
+    assert has_element?(view, "[data-role=post]", post.text)
+  end
 end
